b2968037ee828f56327d44594ff94a9d408e0329
Genilson <genisnilson@gmail.com>
2020-04-30 22:08:48 -0300

    api

---
+++ 2020.1/ProjectTest.API/ProjectTest.API/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectTest.API.Data;
+using ProjectTest.API.Model;
+
+namespace ProjectTest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<User>> ObterTodos()
+        {
+            return UserRepository.users;
+        }
+
+        [HttpPost("registro")]
+        public ActionResult<User> Cadastrar([FromBody] User user)
+        {
+            user.Id = Guid.NewGuid();
+            UserRepository.users.Add(user);
+
+            return user;
+        }
+
+        [HttpDelete("{id}")] 
+        public ActionResult<Guid> Delete(Guid id)
+        {
+            User usuarioEncontrado = UserRepository.users.Find(u => u.Id == id);
+
+            if (usuarioEncontrado == null)
+                return Guid.Empty;
+      
+            UserRepository.users.Remove(usuarioEncontrado);
+            return id;
+        }
+    }
+}
---
+++ 2020.1/ProjectTest.API/ProjectTest.API/Data/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProjectTest.API.Model;
+using System.Collections.Generic;
+
+namespace ProjectTest.API.Data
+{
+    public static class UserRepository
+    {
+        public static List<User> users = new List<User>();
+    }
+}
